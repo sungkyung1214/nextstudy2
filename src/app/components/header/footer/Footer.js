eff2053b72856cdcfde23d2e3b091bd707cd8880
sungkyung1214 <ske703@naver.com>
2023-08-30 11:18:23 +0900

    first

---
+++ src/app/components/header/footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(params) {
+    return(
+        <div style={{width: '80%', margin:'auto',padding:'20px 0px',textAlign:'center', backgroundColor:'lightgray'}}> 
+            Copyright &copy; 2023 한국 ICT 인재개발원
+        </div>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1 id="main">main 창 메인창 메레레레레레</h1>
+    </>
+  )
+}
